9715072083e060b6bed335a87dd0c06f2ee6fda0
Cameron Bray <cameronbray@crescentschool.org>
2016-12-05 09:39:07 -0500

    We skipped chapter 4

---
+++ src/chapter_5/HW5_1.java
@@ -0,0 +1,10 @@
+package chapter_5;
+
+public class HW5_1 {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
---
+++ src/towersOfHanoi/TowersOfHanoiFullCode.java
@@ -0,0 +1,28 @@
+package towersOfHanoi;
+import java.util.*;
+private static Scanner numDisks;
+static private int numberOfDisks;
+static private int[] pole1;
+static private int[] pole2;
+static private int[] pole3;
+
+public void RunDoc{
+
+  numDisks = new Scanner (System.in);
+  system.out.println("How many disks are on your towers on Hanoi");
+  numberOfDisks = numDisks.nextInt();
+
+    for (int i = 1; i <= numberOfDisks; i++){
+          pole1 = new int[numberOfDisks+1];
+          pole1[i] = i;
+    }
+
+  public void validMove (){
+
+    if ((numberOfDisks % 2) == 0){
+      
+    }
+
+  }
+
+}
