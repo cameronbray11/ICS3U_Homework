461416a5637f21ab7e0e4a93cfc5d026af4a14e5
Cameron Bray <cameronbray@crescentschool.org>
2016-12-13 13:55:55 -0500

    ...

--- src/towersOfHanoi/TowersOfHanoi.java
+++ src/towersOfHanoi/TowersOfHanoi.java
@@ -9,56 +9,33 @@ public class TowersOfHanoi {
 	static int num1;
 	static int num2;
 	static int num3;
+	static int numMoves;
 	private static Scanner scanner;
 	static boolean H1 = false;
 	static boolean H2 = false;
 	static boolean H3 = false;
 	public static int [] size;
+	public static int [] disks;
 	
 	
 	public static void main(String[] args) {
-	
-		size [1] = 1;
-		size [2] = 100;
-		size [3] = 100;
 	
 	scanner = new Scanner (System.in);
 	System.out.println("Input the number of Disks : ");
 	numDisks = scanner.nextInt();
 	
 	num1 = numDisks;
+	numMoves = (int) (Math.pow(numDisks, 2)-1);
+
+	disks = new int[numDisks + 1];
+	for (int i = 1; i <= numDisks; i++){
+		disks [i] = 1;
+		System.out.println(disks[i]);
+	}
 	
-	if (num1 )
+	
 	}
 	
 	public void legalMove(){
-		
-		if (H1 == false && H2 == false){
-			
-			if (size[1] < size[2] && size[1] < size[3]){
-				
-			}else if (size[2] < size[1] && size[2] < size[3]){
-				
-			}
-			
-		}
-		if (H2 == false && H3 == false){
-			
-			if (size[3] < size[1] && size[3] < size[2]){
-				
-			}else if (size[2] < size[1] && size[2] < size[3]){
-				
-			}
-			
-		}
-		if (H1 == false && H3 == false){
-			
-			if (size[3] < size[1] && size[3] < size[2]){
-				
-			}else if (size[1] < size[2] && size[1] < size[3]){
-				
-			}
-			
-		}
 	}
 }
